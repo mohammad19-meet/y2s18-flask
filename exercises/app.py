b5bcfacac18c7f8dc6caec31557c824f55f213a4
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 14:10:33 +0300

    Week 2 Day3

--- exercises/app.py
+++ exercises/app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-
+likes_same_sport = True
 @app.route('/')
 def home_page():
-    pass
+    if likes_same_sport == True:
+        return "Hello"
+    else:
+        return render_template("index.html")
+
+
+    
 
 if __name__ == '__main__':
    app.run(debug = True)
